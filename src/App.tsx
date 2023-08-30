a7d0ef6acbbdae2a311305b23cd64f4059d9f762
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-08-30 23:20:05 +0300

    Adding Pages and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Container } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+import { About } from './pages/About';
+import { Navbar } from './components/Navbar';
+
 function App() {
-    return <h1>Hi</h1>;
+    return (
+        <>
+            <Navbar />
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/store" element={<Store />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </Container>
+        </>
+    );
 }
 
 export default App;
